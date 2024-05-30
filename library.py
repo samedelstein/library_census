58528b52141a46b95e1fdad08bfda82ffd3f71a6
Sam Edelstein <sam.i.edelstein@gmail.com>
2024-05-30 05:35:46 -0400

    added requirements

--- library.py
+++ library.py
@@ -1,8 +1,6 @@
-import conda
 import os
 
-conda_file_dir = conda.__file__
-conda_dir = conda_file_dir.split('lib')[0]
+
 proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
 os.environ["PROJ_LIB"] = proj_lib
 
